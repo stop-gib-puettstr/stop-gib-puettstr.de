39432ef5e2ad8e5004b2d43f9fbafa107e288611
Mieyona <lindathiel@hotmail.de>
2023-06-17 18:56:25 +0200

    Zugriff auf style[] über Extension Functions

--- src/main/kotlin/org/puettstr/components/ContentLayout.kt
+++ src/main/kotlin/org/puettstr/components/ContentLayout.kt
@@ -1,14 +1,18 @@
 package org.puettstr.components
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import org.puettstr.util.VaadinExtensions.align
+import org.puettstr.util.VaadinExtensions.backgroundColor
+import org.puettstr.util.VaadinExtensions.borderRadius
+import org.puettstr.util.VaadinExtensions.boxShadow
 
 open class ContentLayout : VerticalLayout() {
 
     init {
-        style["background-color"] = "#fff"
+        backgroundColor = "#fff"
         width = "75%"
-        style["align"] ="center"
-        style["border-radius"] = "5px"
-        style["box-shadow"] = "3px 3px 3px rgba(0,0,0,.2),0px 0px 6px rgba(0,0,0,.2)"
+        align ="center"
+        borderRadius = "5px"
+        boxShadow = "3px 3px 3px rgba(0,0,0,.2),0px 0px 6px rgba(0,0,0,.2)"
     }
 }
